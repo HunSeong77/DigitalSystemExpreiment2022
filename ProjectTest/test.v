11d29c91f77aef511afb86484a634de49e1526bc
HunSeong77 <turtle860347@korea.ac.kr>
2022-11-24 23:52:21 +0900

    LED movement & button switch

--- ProjectTest/test.v
+++ ProjectTest/test.v
@@ -1,60 +1,52 @@
-
-module servo(CLK, ANG, Q);
-// ANG을 DIP SWITCH 1, 2에 넣고
-// SERVO MOTOR의 동작을 확인해보자
-// CLK의 frequency가 충분히 커야함
-    input CLK;
-    input [1:0] ANG;
-    output Q;
-
-    integer cnt;
-    integer pos;
-
-    initial begin
-        cnt = 0;
-        pos = 6 * ANG[1] + 3 * ANG[0];
-    end
-
-    always@(posedge CLK) begin
-        if (cnt < 10) cnt = cnt + 1;
-        else cnt = 0;
-    end
-
-    assign Q = (cnt < pos);
+module SR_FF(CLK, S, R, Q, Q_);
+	input CLK, S, R;
+	output Q, Q_;
+	
+	wire t1, t2;
+	
+	assign t1 = CLK & S;
+	assign t2 = CLK & R;
+	
+	nor(Q, t1, Q_);
+	nor(Q_, t2, Q);
 endmodule
 
-module movingLED(CLK, reset_n, button, LED);
-// reset을 Keypad 0, button을 #에 넣고
-// LED의 동작을 확인해 보자
-    input CLK, reset;
-    input button;
-    output [7:0] LED;
-
-    reg pushed;
-    integer i;
-    initial begin
-        i = 0;
-        pushed = 0;
-    end
-
-    always@(posedge CLK or negedge reset_n) begin
-        if(reset_n == 0) begin
-            i = 0;
-            pushed = 0;
-        end
-        if(button == 1)
-            pushed = 1;
-        if(pushed == 0) begin
-            if(i < 16) i = i + 1;
-            else i = 0;
-    end
-
-    assign LED[0] = (i == 0 || i == 8);
-    assign LED[1] = (i == 1 || i == 9);
-    assign LED[2] = (i == 2 || i == 10);
-    assign LED[3] = (i == 3 || i == 11);
-    assign LED[4] = (i == 4 || i == 12);
-    assign LED[5] = (i == 5 || i == 13);
-    assign LED[6] = (i == 6 || i == 14);
-    assign LED[7] = (i == 7 || i == 15);
+module test(CLK, I, O);
+	input CLK;
+	input [1:0] I;
+	output [7:0] O;
+	
+	wire sr;
+	
+	SR_FF SR1(CLK, I[0], I[1] , sr);
+	LED led(CLK, sr, O);
 endmodule
+
+module counter(CLK, Q);
+	input CLK;
+	output [2:0] Q;
+	reg [3:0] Q;
+	
+	initial Q <= 4'b0000;
+	
+	always@(posedge CLK) begin
+		Q <= Q + 1;
+		end
+endmodule
+
+module LED(CLK, I, O);
+	input CLK, I;
+	output [7:0] O;
+	wire [3:0] cnt;
+	counter c(CLK & I, cnt);
+	
+	assign O[0] = O[1] & !(!cnt[3] & cnt[2] & cnt[1] & !cnt[0]) & !(cnt[3] & !cnt[2] & !cnt[1] & cnt[0]); // 6, 9
+	assign O[1] = O[2] & !(!cnt[3] & cnt[2] & !cnt[1] & cnt[0]) & !(cnt[3] & !cnt[2] & cnt[1] & !cnt[0]); // 5, 10
+	assign O[2] = O[3] & !(!cnt[3] & cnt[2] & !cnt[1] & !cnt[0]) & !(cnt[3] & !cnt[2] & cnt[1] & cnt[0]); // 4, 11
+	assign O[3] = O[4] & !(!cnt[3] & !cnt[2] & cnt[1] & cnt[0]) & !(cnt[3] & cnt[2] & !cnt[1] & !cnt[0]); // 3, 12
+	assign O[4] = O[5] & !(!cnt[3] & !cnt[2] & cnt[1] & !cnt[0]) & !(cnt[3] & cnt[2] & !cnt[1] & cnt[0]); // 7, 8
+	assign O[5] = O[6] & !(!cnt[3] & !cnt[2] & !cnt[1] & cnt[0]) & !(cnt[3] & cnt[2] & cnt[1] & !cnt[0]); // 7, 8
+	assign O[6] = !((!cnt[3] & !cnt[2] & !cnt[1] & !cnt[0]) | (cnt[3] & cnt[2] & cnt[1] & cnt[0])); // 1 ~ 14 0001 , 1110
+	assign O[7] = 1; // 0, 15 0000, 1111
+endmodule
+	
